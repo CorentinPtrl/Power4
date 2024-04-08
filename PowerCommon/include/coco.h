3d7c92c9fbe000dca5764d4c7c77391b74c532f9
Corentin Pitrel <corentin.pitrel@outlook.fr>
2024-04-08 09:45:23 +0200

    Initial Commit

---
+++ PowerCommon/include/coco.h
@@ -0,0 +1,8 @@
+//
+// Created by corentin on 4/8/24.
+//
+
+#ifndef POWER4_COCO_H
+#define POWER4_COCO_H
+
+#endif //POWER4_COCO_H
---
+++ PowerCommon/src/coco.c
@@ -0,0 +1,5 @@
+//
+// Created by corentin on 4/8/24.
+//
+
+#include "../include/coco.h"
